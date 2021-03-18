9331bbdf30e7663bcf4e14337de35d3eaba0295c
ajdo9 <warnek94@gmail.com>
2021-03-18 20:10:49 +0100

    Add and Edit a product

--- routes/products.js
+++ routes/products.js
@@ -1,15 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const uuid = require('uuid');
 
-var products = [
-  {id: 1, title: 'Lorem ipsum', content: 'buy some milk', isEdit: false, isTrue: false},
-  {id: 2, title: 'Lorem ipsum', content: 'play mario kart', isEdit: false, isTrue: false},
-  {id: 3, title: 'Lorem ipsum', content: 'play mario kart', isEdit: false, isTrue: false}
+const products = [
+  {id: uuid.v4(), title: 'Lorem ipsum', content: 'buy some milk', isEdit: false, isTrue: false},
+  {id: uuid.v4(), title: 'Lorem ipsum', content: 'play mario kart', isEdit: false, isTrue: false},
+  {id: uuid.v4(), title: 'Lorem ipsum', content: 'play mario kart', isEdit: false, isTrue: false}
 ]
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({products});
+});
+
+router.post('/', function(req, res, next) {
+  const product = req.body.product;
+  product.id = uuid.v4();
+  products.push(product);
+  res.status(201).json({product});
+});
+
+router.put('/:id', function(req, res, next) {
+  const idx = products.findIndex(product => product.id === req.params.id);
+  products[idx] = req.body.product;
+  res.json({product: req.body.product});
 });
 
 module.exports = router;
